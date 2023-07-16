1ffaa2d1be7b2f68623763c7fd0f3d90e4aaecd3
dennis-adrian <dennisguzmanbo@gmail.com>
2023-07-15 23:30:23 -0300

    Add player surface wrapped in rectangle object for better positioning

--- utils/constants.py
+++ utils/constants.py
@@ -2,9 +2,11 @@ TITLE = "Learning Pygame"
 FPS = 60
 SNAIL_INITIAL_X_POS = 800
 SNAIL_MOVE_SPEED = 4
+GROUND_X_POS = 300
 
 # Assets paths
 SKY = "assets/graphics/Sky.png"
 GROUND = "assets/graphics/ground.png"
 FONT_TYPE = "assets/font/Pixeltype.ttf"
 SNAIL = "assets/graphics/snail/snail1.png"
+PLAYER = "assets/graphics/player/player_walk_1.png"
